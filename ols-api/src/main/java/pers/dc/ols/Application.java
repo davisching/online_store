4b7794732e6c1c52bc97eed4affe40e3bd17a434
景诗维 <davisching@outlook.com>
2019-12-11 17:24:24 +0800

    添加 Mybatis 依赖和配置文件
    同时添加了 HikariCP 的配置

--- ols-api/src/main/java/pers/dc/ols/Application.java
+++ ols-api/src/main/java/pers/dc/ols/Application.java
@@ -1,4 +1,4 @@
-package pers.dc;
+package pers.dc.ols;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ols-api/src/main/java/pers/dc/ols/controller/HelloController.java
+++ ols-api/src/main/java/pers/dc/ols/controller/HelloController.java
@@ -1,4 +1,4 @@
-package pers.dc.controller;
+package pers.dc.ols.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

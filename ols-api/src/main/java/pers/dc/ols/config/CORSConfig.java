c3328e2c1c7ef768b784a7df46c34b525f19d5de
景诗维 <davisching@outlook.com>
2019-12-14 22:28:00 +0800

    添加CROS跨域配置，前端调试成功

---
+++ ols-api/src/main/java/pers/dc/ols/config/CORSConfig.java
@@ -0,0 +1,28 @@
+package pers.dc.ols.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CORSConfig {
+
+    public CORSConfig() {
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:8080");
+        config.setAllowCredentials(true); // 是否支持 cookie
+        config.addAllowedMethod("*"); //允许所有请求
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        
+        return new CorsFilter(source);
+    }
+}

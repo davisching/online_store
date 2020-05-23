2fbe70e8e5bd73e00e56aa8da87e1721758a57a2
Davisching <davisching@outlook.com>
2020-05-23 22:18:39 +0800

    更改ip

--- ols-api/src/main/java/pers/dc/ols/config/CORSConfig.java
+++ ols-api/src/main/java/pers/dc/ols/config/CORSConfig.java
@@ -16,6 +16,13 @@ public class CORSConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOrigin("http://172.17.49.24:8080");
+        config.addAllowedOrigin("http://172.17.49.24");
+//        config.addAllowedOrigin("http://106.15.194.214:8080");
+//        config.addAllowedOrigin("http://106.15.194.214");
+        config.addAllowedOrigin("http://34.80.79.144:8080");
+        config.addAllowedOrigin("http://34.80.79.144");
+        config.addAllowedOrigin("*");
         config.setAllowCredentials(true); // 是否支持 cookie
         config.addAllowedMethod("*"); //允许所有请求
         config.addAllowedHeader("*");
